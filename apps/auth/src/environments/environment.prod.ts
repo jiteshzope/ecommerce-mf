1cd2dffc2d9ab171c483115f4f6133fa8049bb30
Jitesh Zope <jiteshzope26031995@gmail.com>
2026-03-23 18:41:50 +0100

    updated prod env files temporarily to have the local api url to verify all the containerized applications running locally in docker containers

--- apps/auth/src/environments/environment.prod.ts
+++ apps/auth/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  authApiBaseUrl: 'https://your-api-domain/api/v1',
+  authApiBaseUrl: 'http://localhost:3000/api/v1',
 } as const;
--- apps/shell/src/environments/environment.prod.ts
+++ apps/shell/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  ecommerceApiBaseUrl: 'https://your-api-domain/api/v1',
+  ecommerceApiBaseUrl: 'http://localhost:3000/api/v1',
 } as const;
