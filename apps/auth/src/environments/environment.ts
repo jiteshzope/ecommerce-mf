ca38659f15a087e5e34f71d13fef85516258fd95
Jitesh Zope <jiteshzope26031995@gmail.com>
2026-03-24 11:43:02 +0100

    Updated the api url dynamically based on whether its browserHost or localhost in all evn files

--- apps/auth/src/environments/environment.ts
+++ apps/auth/src/environments/environment.ts
@@ -1,4 +1,9 @@
+const localApiBaseUrl = 'http://localhost:3000/api/v1';
+const browserHost = typeof window !== 'undefined' ? window.location.hostname : '';
+const isLocalHost = browserHost === 'localhost' || browserHost === '127.0.0.1';
+
 export const environment = {
   production: false,
-  authApiBaseUrl: 'http://localhost:3000/api/v1',
+  authApiBaseUrl:
+    typeof window !== 'undefined' && !isLocalHost ? `${window.location.origin}/api/v1` : localApiBaseUrl,
 } as const;
