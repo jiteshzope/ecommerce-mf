364a3d4628a94b69e432be42e1262264b10f082e
Jitesh Zope <jiteshzope26031995@gmail.com>
2026-03-26 00:29:26 +0100

    Unit test fixes for Cart

--- apps/cart/src/app/remote-entry/entry.spec.ts
+++ apps/cart/src/app/remote-entry/entry.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { RemoteEntry } from './entry';
 
 describe('RemoteEntry', () => {
@@ -8,6 +9,7 @@ describe('RemoteEntry', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RemoteEntry],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RemoteEntry);
